a54cca7133563a898861fd304b96a7d21c5664a0
Katie Lin <katiekklht@gmail.com>
2021-11-09 12:33:20 +0000

    Refactor with resources, before_action & partial form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+  # before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -16,25 +17,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(strong_params)
 
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
   end
 
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 
   def strong_params
     params.require(:task).permit(:title, :details, :completed)
